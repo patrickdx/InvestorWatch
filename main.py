36189d7e40b3c4298bf2102e8a392bb48323cf65
patrickdx <blah0833@gmail.com>
2023-12-31 21:03:38 -0500

    refactoring into classes

--- main.py
+++ main.py
@@ -35,10 +35,9 @@ def find_sentiment(sentence):
     from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer        # Vader is optimized for social media data and can yield good results when used with data from twitter, facebook, etc.
 
     textblob = TextBlob(sentence)
-    
-    
     analyzer = SentimentIntensityAnalyzer()
-    vader = analyzer.polarity_scores(sentence)      #
+    vader = analyzer.polarity_scores(sentence)      
+
     # https://github.com/cjhutto/vaderSentiment?tab=readme-ov-file#about-the-scoring
     if vader['compound'] >= 0.05  and textblob.polarity >= 0: 
         sentiment = 'positive'
@@ -47,65 +46,76 @@ def find_sentiment(sentence):
         sentiment = 'negative'
 
     else: sentiment = 'netural'
-
     
     avg_polarity = (vader['compound'] + textblob.polarity) / 2
     logger.info(f"{sentiment}, {vader['compound']} , {textblob.polarity}")
     return sentiment, avg_polarity
     
 
-def elastic_index(ticker : str):
-    from elasticsearch.exceptions import BadRequestError
-    index_name = 'stock-%s' % ticker.lower()
-    try: 
-        es.indices.create(index= index_name, body=config.mapping)    
-    except BadRequestError:
-        logger.info(f"index {index_name} already exists")
 
-    return index_name
-
-
-def news_headlines(ticker : str):
-    # TODO: Get a more accurate price quote, based on the time of release of an article.
-    import yfinance as yf    
-    import datetime
-
-    source = 'https://finance.yahoo.com/quote/%s/news?p=%s' % (ticker, ticker)
-    blacklist = ["Motley Fool", "Insider Monkey", "Investor's Business Daily"]
-    stock = yf.Ticker(ticker)
-    index_name = elastic_index(ticker)
+class NewsHeadlineIndexer: 
+    import yfinance as yf 
     
-    logger.info(f"{ticker} news: " + str([news['title'] for news in stock.news]))
+    def __init__(self):
+        source = 'https://finance.yahoo.com/quote/%s/news?p=%s'
 
-    for news in stock.news: 
 
-        # filter out low-quality articles 
-        for word in keywords[ticker]: 
-            if word.lower() in news['title'].lower() and news['publisher'] not in blacklist:
+    def news_headlines(self, ticker):
+        ''' 
+        Indexes news headline document into its corresponding ticker index in elasticsearch. 
+        '''
+        # TODO: Get a more accurate price quote, based on the time of release of an article.
+        
+        import datetime
 
-                epoch = news['providerPublishTime']          # The time the article was published, represented as a Unix timestamp.
-                date = datetime.datetime.fromtimestamp(epoch).isoformat()
+        blacklist = ["Motley Fool", "Insider Monkey"] # "Investor's Business Daily"
+        stock = self.yf.Ticker(ticker)
+        index_name = self.create_index(ticker)
+        
+        logger.info(f"{ticker} news: " + str([news['title'] for news in stock.news]))
 
-                logger.info(f"{date}, {news['title']}, {news['link']}, {news['publisher']}")
-                sentiment = find_sentiment(news['title'])
+        for news in stock.news: 
 
-                # add to elasticsearch, assign unique uuid to prevent duplicates
-                es.index(index = index_name, id = news['uuid'], 
-                    body = {
-                        'date': date, 
-                        'title': news['title'],
-                        'polarity': sentiment[1],
-                        'sentiment': sentiment[0],
-                        'url': news['link'],
-                        'publisher' : news['publisher'],
-                        'price': stock.info['currentPrice']     
-                    })
-                
-                break 
+            # filter out low-quality articles 
+            for word in keywords[ticker]: 
+                if word.lower() in news['title'].lower() and news['publisher'] not in blacklist:
+
+                    epoch = news['providerPublishTime']          # The time the article was published, represented as a Unix timestamp.
+                    date = datetime.datetime.fromtimestamp(epoch).isoformat()
+
+                    logger.info(f"{date}, {news['title']}, {news['link']}, {news['publisher']}")
+                    sentiment = find_sentiment(news['title'])
+
+                    # add to elasticsearch, assign unique uuid to prevent duplicates
+                    es.index(index = index_name, id = news['uuid'], 
+                        body = {
+                            'date': date, 
+                            'title': news['title'],
+                            'polarity': sentiment[1],
+                            'sentiment': sentiment[0],
+                            'url': news['link'],
+                            'publisher' : news['publisher'],
+                            'price': stock.info['currentPrice']     
+                        })
+                    
+                    break 
+
+
+
+    def create_index(self, ticker):
+        from elasticsearch.exceptions import BadRequestError
+        index_name = 'stock-%s' % ticker.lower()
+        try: 
+            es.indices.create(index= index_name, body=config.mapping)    
+        except BadRequestError:
+            logger.debug(f"index {index_name} already exists")
+
+        return index_name        
+
+    def stock_quote(ticker, timestamp): 
 
         
-
-
+        pass          
 
 
 
@@ -119,22 +129,19 @@ def news_headlines(ticker : str):
     
 # news_headlines('NVDA')
 
+
+# Setup elasticsearch, uncomment if you are using the cloud version. 
+
 # es = Elasticsearch(     # for cloud subscription
 #     cloud_id = config.cloud_id,
 #     basic_auth =('elastic', config.password)
 # )
 
-
 from elasticsearch import Elasticsearch
-
 es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200, 'scheme': 'http'}],
                 basic_auth =(config.es_user, config.es_password))
 
 
-
-# populate elasticsearch indices with news documents 
-for stock in keywords: 
-    news_headlines(stock)
 
 def viewIndex(index): 
     ''' 
@@ -153,4 +160,9 @@ def clear_indexes():
         es.indices.delete(index=index_name, ignore=[400, 404])
 
     logger.info("All indices deleted.")
+
+if __name__ == '__main__':      
+    # populate elasticsearch indices with news documents 
+    for stock in keywords: 
+        news_headlines(stock)
 
