602d7f1519e3297dd1756e8ab415566a3ce55543
patrickdx <blah0833@gmail.com>
2024-01-27 15:24:50 -0500

    updated reddit indexer

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ import datetime
 import time
 import nltk
 
-logging.basicConfig(format = '')
-logger = logging.getLogger("yfinance")
-logger.setLevel(logging.INFO)
-
-
 
 def find_sentiment(sentence):   # TODO: Improve accuracy of these models or use a self-made one
     '''
@@ -51,58 +46,45 @@ class NewsHeadlineIndexer:
         self.filtered =0 
         self.accepted = 0
 
-        # self.keywords = config.keywords 
-        self.keywords = {
-            'AAPL': ['APPL', '$APPL', 'Apple', 'IPhone'], 
-            'NVDA': ['NVDA', '$NVDA', 'Nvidia', 'CUDA', 'Artifical Intelligence', 'Jensen Huang'],
-            'MSFT': ['MSFT', '$MSFT', 'Microsoft', 'Satya Nadella', 'OpenAI'],
-            'GOOGL': ['GOOGL', '$GOOG', '$GOOGL', 'Google', 'Alphabet', 'Gemini'],
-            'TSLA': ['TSLA', '$TSLA', 'Tesla', 'Cybertruck', 'Starlink'],
-            'AMZN': ['AMZN', '$AMZN', 'Amazon', 'Jeff Bezos', 'Prime'] 
-        }
-
-        self.blacklist = ["Motley Fool", "Insider Monkey", "Investor's Business Daily"]
-
-
-
+        self.stocks = config.tickers
+        self.blacklist = config.author_blacklist
+        
 
     def news_headlines(self, ticker):
         ''' 
-        Indexes news headline document into its corresponding ticker index in elasticsearch. 
+        Indexes news headline documents using yfinance into its corresponding ticker index in elasticsearch. 
         '''
-        
-
         stock = self.yf.Ticker(ticker)
         index_name = self.create_index(ticker)
         
-        logger.info(f"\n{ticker} news: " + str([news['title'] for news in stock.news]))
+        yf_logger.info(f"\n{ticker} news: " + str([news['title'] for news in stock.news]))
 
         for news in stock.news: 
 
             # filter out low-quality articles, subject to change
             # tokens = nltk.word_tokenize(news['title'])
-            match_tokens = self.keywords[ticker]
+            match_tokens = self.stocks[ticker]
 
-            for word in self.keywords[ticker]:                
+            for word in self.stocks[ticker]:                
                 if word.lower() in news['title'].lower() and news['publisher'] not in self.blacklist:
 
-                    logger.info('\n')
+                    yf_logger.info('\n')
                     epoch = news['providerPublishTime']          # The time the article was published, represented as a Unix timestamp.
                     date = datetime.datetime.fromtimestamp(epoch) 
 
-                    logger.info(f"{date}, {news['title']}, {news['link']}, {news['publisher']}")
+                    yf_logger.info(f"{date}, {news['title']}, {news['link']}, {news['publisher']}")
                     sentiment = find_sentiment(news['title'])
 
                     if sentiment['subjectivity'] >= 0.75: 
                         # Avoid: Magnificent Seven Stocks To Buy And Watch: Nvidia Stock Hits More Record Highs
-                        logger.info("Article is too subjective, discarding...")
+                        yf_logger.info("Article is too subjective, discarding...")
                         self.filtered += 1
                         break
                    
                     stockPrice = self.stock_quote(stock, date)
 
                     # add to elasticsearch, assign unique uuid to prevent duplicates
-                    logger.info(es.index(index = index_name, id = news['uuid'], 
+                    yf_logger.info(es.index(index = index_name, id = news['uuid'], 
                         body = {
                             'date': date.isoformat(), 
                             'title': news['title'],
@@ -126,26 +108,10 @@ class NewsHeadlineIndexer:
         try: 
             es.indices.create(index= index_name, body=config.mapping)    
         except BadRequestError:
-            logger.debug(f"index {index_name} already exists")
+            yf_logger.debug(f"index {index_name} already exists")
 
         return index_name        
 
-    def stock_quote(self, stock, date : datetime):           
-        '''
-        TODO: Get a more accurate price quote, based on the time of release of an article.
-        '''
-        date = date.replace(second = 0)         # strip leading seconds 
-        df = stock.history(start = date, interval = '1m')
-
-        if len(df) > 0: 
-            price_quote = df[df.index.time == date.time()].iloc[0]        # filter for time of published article
-            print(date, price_quote, price_quote['Close'])
-            # print(type(price_quote))
-            return round(price_quote['Close'], 2)
-
-        else:   # return last close price if markets are closed
-            return round(stock.info['currentPrice'], 2)
-        
 
 
 class RedditIndexer: 
@@ -155,18 +121,35 @@ class RedditIndexer:
             client_secret = config.client_secret,
             user_agent= config.user_agent
         )    
+        self.stocks = config.tickers
 
-    def get_posts(subreddit):
-        for submission in reddit.subreddit("stocks").top(time_filter="month"):
-            for stock in whitelist:
-                for word in whitelist[stock]: 
-                    if word in submission.title:
-                        # reddit_log.info(f'{stock}: {submission.title}')
-                        find_sentiment(submission.title)
+    def get_posts(self, subreddit):
+        reddit = self.reddit 
+        for submission in reddit.subreddit(subreddit).top(time_filter="month"):
+            for ticker, keywords in self.stocks.items():
+                for word in keywords:  
+                    if word in submission.title:    #  index here
+
+                        reddit_logger.info(f'{ticker}: {submission.title}')
+                        sentiment = find_sentiment(submission.title)
+                        index_name = self.create_index(ticker)
+                        date = datetime.datetime.fromtimestamp(submission.created_utc)          # created_utc is unix timestamp 
+
+                        # add to elasticsearch, assign unique uuid to prevent duplicates
+                        yf_logger.info(es.index(index = index_name, id = submission.id, 
+                            body = {
+                                'date': date.isoformat(), 
+                                'title': submission.title,
+                                'polarity': sentiment['polarity'],
+                                'sentiment': sentiment['sentiment'],
+                                'url': submission.url, 
+                                'publisher' : submission.author.name,
+                                'price': stock_quote(ticker)      
+                            }))
+
+                        self.accepted += 1
+                        time.sleep(0.5)
                         break 
-                        #  index here
-                
-
 
 
 # Setup elasticsearch, uncomment if you are using the cloud version. 
@@ -189,7 +172,7 @@ def viewIndex(index):
     '''
     elasticsearch_url = "http://%s:%s@localhost:9200%s/_search?pretty"  % (config.es_user, config.es_password, index)
     response = requests.get(elasticsearch_url) 
-    print(response.text)
+    print(response.text)        
 
 def clear_indexes():
     indices = es.indices.get_alias(index = "*")
@@ -200,11 +183,43 @@ def clear_indexes():
 
     logger.info("All indices deleted.")
 
+
+def stock_quote(self, stock : Ticker, date : datetime = None):           
+    '''
+    Gets a more accurate price quote, based on the time of release of an article.
+    '''
+    
+    if date: date = date.replace(second = 0)         # strip leading seconds 
+    df = stock.history(start = date, interval = '1m')
+
+    if len(df) > 0: 
+        price_quote = df[df.index.time == date.time()].iloc[0]        # filter for time of published article
+        print(date, price_quote, price_quote['Close'])
+        # print(type(price_quote))
+        return round(price_quote['Close'], 2)
+
+    else:   # return last close price if markets are closed
+        return round(stock.info['currentPrice'], 2)
+        
+
+
 if __name__ == '__main__':      
     # populate elasticsearch indices with news documents 
-    news = NewsHeadlineIndexer()
+    
 
-    for stock in news.keywords: 
-        news.news_headlines(stock)      # TODO: add summary of articles added to each index
+    # setup logging 
+    logging.basicConfig(format = '') 
 
+    logger = logging.getLogger("investorwatch")
+    logger.setLevel(logging.INFO)
+    yf_logger = logging.getLogger("yfinance")
+    yf_logger.setLevel(logging.INFO)
+    reddit_logger = logging.getLogger("reddit")
+    reddit_logger.setLevel(logging.INFO)
+    
+
+    headlines = NewsHeadlineIndexer()
+    reddits = RedditIndexer() 
+    for t in headlines.stocks:
+        headlines.news_headlines(t)          # TODO: add logging summary of articles added to each index
 
