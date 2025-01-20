0544f06330bde0ec9e03d81d8499f0582c95f576
patrickdx <blah0833@gmail.com>
2025-01-19 20:53:00 -0500

    refactor ddx

--- enrich.py
+++ enrich.py
@@ -1,99 +1,60 @@
-import logging
-import requests 
-from datetime import timedelta, datetime
-import time
-import pandas as pd
 import yfinance as yf 
 from bs4 import BeautifulSoup
-import requests
-import re 
-import constants
-import sys
+import time
 import os 
 import numpy as np 
-
+from finvizfinance.news import News
+from finvizfinance.screener.overview import Overview
+from finvizfinance.quote import finvizfinance
+import pandas as pd 
+import yfinance as yf
+from datetime import timedelta, datetime
+import time
 
 INPUT_PATH = 'stocks/tickers'
-OUTPUT_PATH = 'stocks/tickers_e'
 
 class explore:
-
-    def __init__(self, stock):
-        self.stock = stock 
-        PATH = OUTPUT_PATH if os.path.exists(f'{OUTPUT_PATH}/{stock}.csv') else INPUT_PATH      # why mine a second time?
-        self.news = pd.read_csv(f'{PATH}/{stock}.csv', parse_dates=['Date'])
-
-    def mine_price_changes(self):
-        '''
-        Adds intraday price movement to the news dataframe
-        '''
-        def get_intraday_return(): 
-            oldest_date = self.news['Date'].min()
-            data = yf.download(self.stock, period = 'max', start = oldest_date - timedelta(weeks = 1))       # news from [oldest_date, today]
-            data.columns = data.columns.droplevel(1)                             # drop the multi-level column index
-            data.columns.name = None 
-
-            data['Prev Close'] = data['Close'].shift(1)     # get previous close by shifting it down by 1
-            data['% Change'] = ((data['Close'] - data['Prev Close']) / data['Prev Close'])  * 100
-            data['% Change'] = data['% Change'].round(2)
-            data.sort_values('% Change', ascending = False, inplace = True)
-            return data
-
-        def lookup_date(timestamp, df):     # Looks up the price movement on a given date and populates news df
-            try:
-                return df.loc[str(timestamp.date()), '% Change']
-            except KeyError:
-                return None     # print(f'No price movement data for {timestamp.date()}')
-
-        price_changes = get_intraday_return() 
-        self.news['% Change'] = self.news['Date'].apply(lambda x: lookup_date(x, price_changes) )   
-      
-
-    def mine_google_results(self, topn = 1): 
-        '''
-        Mines potential news catalysts for the top N days with the highest price change (only necessary b/c of the free google api limit)
-        '''
-        news = self.news
-        
-        def get_search_results(row):                 
-            if 'hits' in row and not np.isnan(row['hits']): print("already mined!"); return row['hits']           
-            
-            query = row['Title']
-        
-            params = {
-                'q': f'"{query}"',
-                'key': constants.GOOGLE_API_KEY, 
-                'cx': constants.SEARCH_ENGINE_ID, 
-            }
-
-            try: 
-                response = requests.get('https://www.googleapis.com/customsearch/v1', params = params).json() 
-                if 'totalResults' not in response['queries']['request'][0]: return 0 
-                num_results = int(response['queries']['request'][0]['totalResults'])
-                print(query, num_results)
-                return num_results
-            
-            except Exception as e:
-                print(response)
-                raise e
-
-        volatility = news['% Change'].drop_duplicates().dropna().sort_values(key=abs, ascending=False).head(topn)
-        news.loc[news['% Change'].isin(volatility), 'hits'] = news.loc[news['% Change'].isin(volatility)].apply(get_search_results, axis = 1)
-
+    '''A collection of data exploration ideas to be applied on the base news article df.'''
     
+    def __init__(self, ticker):
+        self.ticker = ticker 
+        self.df = self._get_news()
+
+
+    def _get_news(self):       # sinks new news sources to csv files
+        ticker = self.ticker
+        try:
+            time.sleep(0.2)
+            whitelist = ['Bloomberg', 'Reuters']
+            news_df = finvizfinance(ticker).ticker_news()
+            news_df = news_df[news_df['Source'].isin(whitelist)]              # whitelist
+
+            file = f"{"./stocks/tickers"}/{ticker}.csv"
+            last_modified = datetime.fromtimestamp(os.path.getmtime(file))
+            news_df = news_df[(news_df['Date'] >= last_modified)].sort_values('Date')       # filter new news inbetween modified date and today
+
+            print(f'Found {len(news_df)} new sources of ${ticker} ...')   
+            return news_df
+
+        except Exception as e:
+            print(e)
     
-    def enrich(self, topn = 1):
-        '''
-        Enriches the news dataframe with intraday price changes and google search results
-        '''
-        self.mine_price_changes() 
-        self.mine_google_results(topn)
-        # print(self.news)
-        self.news.to_csv(f'{OUTPUT_PATH}/{self.stock}.csv', index = False)
+
+    def _ex_price_changes(self): 
+        pass
+
+    def _ex_search_hits(self):
+        pass
+
+    def enrich(self):               
+        
+        self._ex_price_changes()
+        self._ex_search_hits()
+        return self.df 
+        
 
 
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    for stock in args: explore(stock.upper()).enrich()
+print(explore('AAPL').df)
+        
 
 
--- stocks.py
+++ stocks.py
@@ -13,53 +13,43 @@ PATH = 'stocks/tickers'
 if not os.path.exists(PATH): os.makedirs(PATH)
 
 
-df = pd.read_csv('stocks/stocks.csv')
+stock_list = pd.read_csv('stocks/stocks.csv')
 
-def update_stocklist():
-    # Get list of stocks that meet market cap criteria. Since sp500 is self-cleansing, companies get aquired / delisted frequently.
-
-    market_cap = {
-            'Mega' : 'Mega ($200bln and more)',
-            'Large' : '+Large (over $10bln)',
-            'Mid' : '+Mid (over $2bln)',
-    }
+def update_stocklist():   # Update the list of stocks that meet market cap criteria.
+    global stock_list
 
     foverview = Overview()
-    filters_dict = {'Market Cap.': market_cap['Large'], 'Country': 'USA'}
-
-    foverview.set_filter(filters_dict=filters_dict)
+    foverview.set_filter(filters_dict={'Market Cap.': '+Large (over $10bln)', 'Country': 'USA'})
     df = foverview.screener_view() 
     df['Market Cap (billions)'] = df['Market Cap'] / 1e9
     df.drop(columns = ['P/E', 'Change', 'Market Cap', 'Volume'], inplace = True)
 
-    df.to_csv('stocks/stocks.csv', index = False)                               # sink all marketcap+ stocks
+    stock_list = pd.concat([df,stock_list]).drop_duplicates(subset=['Ticker'])        # dropped duplicates will be the older ones (in stock_list)
+    stock_list.to_csv('../stocks/stocks.csv', index = False) 
 
 
 def get_news(df : pd.Series):       # sinks new news sources to csv files
     try:
-            
-        time.sleep(0.3)
-        file = f"{PATH}/{df['Ticker']}.csv"
+        time.sleep(0.2)
         whitelist = ['Bloomberg', 'Reuters']
         news_df = finvizfinance(df['Ticker']).ticker_news()
         news_df = news_df[news_df['Source'].isin(whitelist)]              # whitelist
 
-        today = datetime.today()                                           
+        file = f"{STOCK_PATH}/{df['Ticker']}.csv"
         last_modified = datetime.fromtimestamp(os.path.getmtime(file))
-        news_df = news_df[(news_df['Date'] >= last_modified) & (news_df['Date'] <= today)].sort_values('Date')      # look for news dates inbetween last modified date and today
+        news_df = news_df[(news_df['Date'] >= last_modified)].sort_values('Date')       # filter new news inbetween modified date and today
 
         if not os.path.exists(file):
             news_df.to_csv(file, index = False)  
             print(f'created {file} ...')
 
-        news_df.to_csv(file, mode='a', header=False, index=False)         # append/remove header
-        print(f'found {len(news_df)} new sources to {file} ...')   
+        news_df.to_csv(file, mode='a', header=False, index=False)         # append to existing file
+        print(f'Found {len(news_df)} new sources of ${df['Ticker']} ...')   
 
     except Exception as e:
         print(e)
 
-update_stocklist()    
-df.apply(get_news, axis = 1)            # apply get_news() on every row
 
+df.apply(get_news, axis = 1)            # apply get_news() on every row
 
 
